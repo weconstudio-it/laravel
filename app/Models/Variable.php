35a72e0b164fea701e6ad3737dd95e60340ecb01
Diego Verì <solidboss@gmail.com>
2016-06-29 10:57:34 +0200

    aggiunta tabella variable

---
+++ app/Models/Variable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Base\Variable as BaseVariable;
+
+/**
+ * Skeleton subclass for representing a row from the 'variable' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Variable extends BaseVariable
+{
+
+}
